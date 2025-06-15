cf172064f67110b1aa51eaa06518f7cc0c1d9bb6
swift <sixfold.54-county@icloud.com>
2025-06-15 19:55:14 +0100

    Initial Commit

---
+++ Program 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Program 1
+//
+//  Created by swift on 15/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Program 1/Program_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Program_1App.swift
+//  Program 1
+//
+//  Created by swift on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Program_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
