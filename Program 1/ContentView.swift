c4978f1b691c8060b5de39d0f71e7dc4d5d6ea3c
swift <sixfold.54-county@icloud.com>
2025-06-15 20:37:58 +0100

    First simple app to compare x and y

--- Program 1/ContentView.swift
+++ Program 1/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var xInput: String = ""
+    @State private var yInput: String = ""
+    @State private var result: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("This is Swift using SwiftUI")
+        VStack(spacing: 16) {
+            TextField("Enter X", text: $xInput)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            TextField("Enter Y", text: $yInput)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            Button("Compare") {
+                if let x = Int(xInput), let y = Int(yInput) {
+                    if x > y {
+                        result = "X is greater than Y"
+                    } else if x < y {
+                        result = "X is less than Y"
+                    } else {
+                        result = "X is equal to Y"
+                    }
+                } else {
+                    result = "Please enter valid numbers for X and Y."
+                }
+            }
+            .padding(.top)
+
+            Text(result)
+                .foregroundColor(.blue)
+                .padding()
         }
-        .padding()
     }
 }
 
